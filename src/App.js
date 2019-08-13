681640bf64172bebbb7de9793393cac2c212ed7a
arielbk <buchwald.kearney@gmail.com>
2019-08-13 20:30:01 +0200

    Add flip animation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+// import styled from 'styled-components';
+import {useSpring, animated as a} from 'react-spring';
+
+// const Card = styled.section`
+//   width: 80%;
+//   max-width: 800px;
+//   background: #fff;
+//   margin: 6rem auto;
+//   padding: 6rem;
+//   box-shadow: 0 2px 50px rgba(0,0,0,0.05);
+
+//   transform-style: preserve-3d;
+//   transform-origin: center right;
+
+//   transition: 1s;
+//   &:hover {
+//     transform: translateX(-100%) rotateY(-180deg);
+//   }
+// `;
+
+function Card() {
+  const [flipped, set] = useState(false)
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg) scale(${flipped ? 10 : 1})`,
+    config: { mass: 5, tension: 500, friction: 80 }
+  })
+  return (
+    <div onClick={() => set(state => !state)}>
+      <a.div class="c back" style={{ opacity: opacity.interpolate(o => 1 - o), transform }} />
+      <a.div class="c front" style={{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`) }} />
+    </div>
+  )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  // const props = useSpring({transform: 'translateX(-100%) rotateY(-180deg)', from: { translate: 'none', transformStyle: 'preserve-3d', transformOrigin: 'center right'} });
+  // return <Card>Spring</Card>
+  // return <animated.div style={props}>I will fade in</animated.div>
+  // return <Card />
+  return <div onClick={() => set(state => !state)}>
+      <a.div class="c back" style={{ opacity: opacity.interpolate(o => 1 - o), transform }} />
+      <a.div class="c front" style={{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`) }} />
     </div>
-  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom'
+import React, { useState } from 'react'
+import { useSpring, animated as a } from 'react-spring'
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Card() {
+  const [flipped, set] = useState(false)
+  const { transform, opacity, boxShadow } = useSpring({
+    boxShadow: '3px 0 60px rgba(0,0,0,0.0.07)',
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg) scale(${flipped ? 1 : 1.1})`,
+    config: { mass: 5, tension: 300, friction: 50 },
+  })
+  return (
+    <div onClick={() => set(state => !state)}>
+      <a.div className="c back" style={{ opacity, boxShadow: boxShadow.interpolate(o => 1 - o), transform }}>imgimgimg </a.div>
+      <a.div className="c front" style={{ opacity: opacity.interpolate(o => 1 - o), boxShadow, transform: transform.interpolate(t => `${t}`) }}>Actual time table info text</a.div>
+    </div>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<Card />, document.getElementById('root'))
